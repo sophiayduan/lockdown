a2fa999e90c86266d270d2f41911b73d4e5177cc
chairzihan <146217784+chairzihan@users.noreply.github.com>
2025-04-26 16:49:00 -0400

    ANIMATIONS

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ArmController : MonoBehaviour {
+    private Animator animator;
+    public float walkThreshold = 0.1f;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        // Check if player is moving (WASD or joystick input)
+        float moveInput = Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"));
+        bool isWalking = moveInput > walkThreshold;
+
+        animator.SetBool("IsWalking", isWalking);
+    }
+}
